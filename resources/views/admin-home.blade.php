ac8113a656af077b12b2afa90b6f12f3c3511efc
tweacktech <daridatongs@gmail.com>
2023-01-12 18:42:38 +0100

    first commit

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -9,6 +9,7 @@
 
                 <div class="card-body">
                     <div class="row"> <div class="col-md-6" id="m">  
+           
              <img src="{{url('admin.jpeg')}}" class="img-circle" alt="Cinque Terre" width="304" height="236"><div class="row"><center>ADMIN</center> </div>
         </div></div>
                     @if (session('status'))
